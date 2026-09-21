22bb47e6e989144adff4f159e1b79f22ed334885
arunsai-535 <arunsai535@gmail.com>
2026-09-21 12:17:17 +0530

    password validation issue fix

--- backend/models/user.js
+++ backend/models/user.js
@@ -13,6 +13,10 @@ const User = sequelize.define('User', {
     unique: true,
     allowNull: false,
   },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+  },
   mobile: {
     type: DataTypes.STRING(15),
     unique: true,
